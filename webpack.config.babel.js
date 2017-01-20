b97388a3a3cb3372f9a616aaf7d5f55cd67b7fd0
Kees Kluskens <kees@webduck.nl>
2017-01-20 15:25:27 +0100

    upgrade webpack to v2

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,14 +1,14 @@
-import path from 'path';
-import webpack from 'webpack';
-import HtmlWebPackPlugin from 'html-webpack-plugin';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import autoprefixer from 'autoprefixer';
-import { execSync } from 'child_process';
-import dotenv from 'dotenv';
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { execSync } = require('child_process');
+const dotenv = require('dotenv');
 
 dotenv.config({ silent: true });
 
 const IS_DEBUG = process.env.PLUFF_DEBUG === 'true';
+const IS_DEV_SERVER = process.argv[1].indexOf('webpack-dev-server') >= 0;
 
 console.log(`Building for ${IS_DEBUG ? 'DEVELOPMENT' : 'production'}!`);
 
@@ -23,11 +23,13 @@ const plugins = [
   new HtmlWebPackPlugin({
     inject: 'head',
     template: 'app/index.htm',
-    // Relative to `output.publicPath`.
-    filename: '../index.html',
+    // `output.publicPath` is "static/" when not using the dev-server,
+    // but we want the index.html one level above it.
+    filename: `${IS_DEV_SERVER ? '' : '../'}index.html`,
     commitHash,
   }),
-  new ExtractTextPlugin('[name]-[contenthash:7].css', {
+  new ExtractTextPlugin({
+    filename: '[name]-[contenthash:7].css',
     allChunks: true,
   }),
   // Define globals that get inserted in plugin.
@@ -37,14 +39,12 @@ const plugins = [
 ];
 
 if (!IS_DEBUG) {
-  plugins.push(new webpack.optimize.DedupePlugin());
   plugins.push(new webpack.optimize.UglifyJsPlugin({
     // UglifyJs produces nonsense warnings by default.
     compress: { warnings: false },
     // Mangling fucks up Angular.
     mangle: false,
   }));
-  plugins.push(new webpack.optimize.OccurenceOrderPlugin());
 }
 
 export default {
@@ -53,71 +53,59 @@ export default {
     bundle: 'boot.js',
   },
   devtool: IS_DEBUG ? '#eval' : null,
-  debug: IS_DEBUG,
   output: {
     filename: '[name]-[hash:7].js',
     chunkFilename: '[name]-[id]-[chunkhash:7].js',
     path: path.join(__dirname, 'dist/static'),
-    publicPath: 'static/',
+    publicPath: IS_DEV_SERVER ? '/' : 'static/',
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
       }, {
         // Nasty hack to work around a bug in html-webpack-plugin / extract-text-webpack-plugin,
         // where the output is wrong if you're using an inline loader.
         test: /\.htm$/,
         loader: 'underscore-template-loader',
-        query: {
+        options: {
           // html attributes that should be parsed as module.
           attributes: ['img:src', 'link:href'],
         },
       }, {
         test: /\.html$/,
-        loader: 'raw',
+        loader: 'raw-loader',
       }, {
         test: /\.scss$/,
         // First compile with Sass and then Postcss.
-        loader: ExtractTextPlugin.extract(
-            'css?sourceMap!' +
-            'postcss!' +
-            'sass?sourceMap&outputStyle=compressed',
-            // Paths in CSS are relative to dist/static/ instead of dist/
-            { publicPath: '' }
-        ),
-      }, {
-        test: /\.json$/,
-        loader: 'json',
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader?sourceMap!' +
+            'postcss-loader!' +
+            'sass-loader?sourceMap&outputStyle=compressed',
+          // Paths in CSS are relative to dist/static/ instead of dist/
+          publicPath: '',
+        }),
       }, {
         // Extract all non-CSS and non-JS assets.
         test: /\.(gif|png|jpe?g|svg|ico|woff|ttf)$/i,
-        loader: 'file',
-        query: {
+        loader: 'file-loader',
+        options: {
           name: '[name]-[hash:7].[ext]',
         },
       },
     ],
   },
   plugins,
-  resolveLoader: {
-    root: path.join(__dirname, 'node_modules'),
-  },
   resolve: {
-    root: path.join(__dirname, 'app'),
-    extensions: ['', '.js'],
+    modules: [path.join(__dirname, 'app'), 'node_modules'],
     alias: {
       'angular-translate-cookie': 'angular-translate/dist/angular-translate-storage-cookie/angular-translate-storage-cookie',
     },
   },
-  postcss() {
-    return [
-      autoprefixer({ browsers: ['last 1 versions'] }),
-    ];
-  },
   devServer: {
+    stats: 'errors-only',
     historyApiFallback: true,
   },
 };
